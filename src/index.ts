0c2bc3cff7ea4d6541c233f255eb36c79836cd13
htetaungkhant <htetaung23.ha@gmail.com>
2023-07-03 11:18:17 +0630

    change response base URL

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,8 @@ app.use(cors({
 }));
 
 app.get('/api/v1', (req: Request, res: Response) => {
-  res.send('<html><head><title>Document</title><body><h1>Welcome From Ecommerce API(version 1)</h1><a href=https://github.com/HtetAungKhant23/E-Commerce-API.git>Source Code</a></body></html>');
+  // res.send('<html><head><title>Document</title><body><h1>Welcome From Ecommerce API(version 1)</h1><a href=https://github.com/HtetAungKhant23/E-Commerce-API.git>Source Code</a></body></html>');
+  res.redirect('https://api-doc-ecommerce.onrender.com/doc.html');
 });
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/auth', authRouter);
